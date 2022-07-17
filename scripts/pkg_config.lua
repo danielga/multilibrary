b4b2c3f97eb5039d2ac31f19e08d6938520b1037
Daniel <danielga@outlook.pt>
2022-07-17 19:06:04 +0100

    Cleaned up and improved pkg-config wrapper

--- scripts/pkg_config.lua
+++ scripts/pkg_config.lua
@@ -1,43 +1,26 @@
+local function is_non_empty_string(value)
+	return type(value) == "string" and #value ~= 0
+end
+
 if os.istarget("windows") then
 	function pkg_config() end
-
-	return
-end
-
-local pkg_config_parsers = {
-	l = function(lib)
-		links(lib)
-	end,
-	L = function(libsfolder)
-		syslibdirs(libsfolder)
-	end,
-	I = function(incfolder)
-		sysincludedirs(incfolder)
-	end,
-	D = function(define)
-		defines(define)
-	end
-}
-
-function pkg_config(cmds)
-	local shouldlink = false
-	local libs = {}
-	for i = 1, #cmds do
-		if cmds[i] == "--libs" then
-			shouldlink = true
-		elseif cmds[i]:sub(1, 1) ~= "-" then
-			table.insert(libs, cmds[i]:match("^lib(.+)$") or cmds[i])
+elseif is_non_empty_string(os.outputof("pkg-config --version")) then
+	local pkg_config_parsers = {
+		l = function(lib)
+			links(lib)
+		end,
+		L = function(libsfolder)
+			syslibdirs(libsfolder)
+		end,
+		I = function(incfolder)
+			sysincludedirs(incfolder)
+		end,
+		D = function(define)
+			defines(define)
 		end
-	end
+	}
 
-	if shouldlink then
-		links(libs)
-	end
-end
-
-local version = os.outputof("pkg-config --version")
-if version and version ~= "" then
-	pkg_config = function(cmds)
+	function pkg_config(cmds)
 		local output = os.outputof("pkg-config --silence-errors " .. table.concat(cmds, " "))
 		if not output or output == "" then
 			return
@@ -53,5 +36,21 @@ if version and version ~= "" then
 		end
 	end
 else
-	print("pkg-config doesn't exist, going with the default behavior of premake links")
+	print("pkg-config isn't installed, going with the default behavior of premake links")
+
+	function pkg_config(cmds)
+		local shouldlink = false
+		local libs = {}
+		for i = 1, #cmds do
+			if cmds[i] == "--libs" then
+				shouldlink = true
+			elseif cmds[i]:sub(1, 1) ~= "-" then
+				table.insert(libs, cmds[i]:match("^lib(.+)$") or cmds[i])
+			end
+		end
+
+		if shouldlink then
+			links(libs)
+		end
+	end
 end
